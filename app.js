68aab7d343cae53e351084155a68008b86aa1e81
Arjun <arjunsingh8112@gmail.com>
2022-06-24 10:10:12 +0530

    Optional: password check

---
+++ app.js
@@ -0,0 +1,8 @@
+const password = document.getElementById('pass')
+const passwordConfirmation = document.getElementById('pass-2')
+
+
+// while (password.textContent !== passwordConfirmation.textContent) {
+console.log('incorrect pass');
+// }
+
